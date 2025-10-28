53a0dd85ebce395ad3b5ce7c114845e3c5953009
Ibtihale-meftah <ibtihalmeftah.120@gmail.com>
2025-10-28 14:15:09 +0100

    Add files via upload

---
+++ GestionTaches.java
@@ -0,0 +1,24 @@
+package tasks;
+
+
+import java.util.LinkedList;
+
+public class GestionTaches {
+    private LinkedList<Tache> taches = new LinkedList<>();
+
+    public void ajouterTache(Tache tache) {
+        taches.add(tache);
+    }
+
+    public void terminerPremiereTache() {
+        if (!taches.isEmpty()) {
+            taches.getFirst().terminer();
+        }
+    }
+
+    public void afficherTaches() {
+        for (Tache t : taches) {
+            System.out.println(t);
+        }
+    }
+}
